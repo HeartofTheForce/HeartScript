8be46043e120f4561106f033473fab63ab63c678
Heart <>
2021-03-08 16:25:17 +0200

    move nodebuilder move operatorinfo

--- src/HeartScript/Parsing/NodeBuilder.cs
+++ src/HeartScript/Parsing/NodeBuilder.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using HeartScript.Parsing;
+using HeartScript.Nodes;
 
-namespace HeartScript.Nodes
+namespace HeartScript.Parsing
 {
     public class NodeBuilder
     {
--- src/HeartScript/Parsing/OperatorInfo.cs
+++ src/HeartScript/Parsing/OperatorInfo.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using HeartScript.Parsing;
+using HeartScript.Nodes;
 
-namespace HeartScript.Nodes
+namespace HeartScript.Parsing
 {
     public delegate INode BuildNode(Token token, INode? leftNode, IReadOnlyList<INode> rightNodes);
 
