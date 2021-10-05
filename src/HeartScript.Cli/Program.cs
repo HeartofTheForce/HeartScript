0dc869bf9032533dbf788a550896a0e070c29f61
heart <>
2021-10-05 10:02:36 +0200

    sanity

--- src/HeartScript.Cli/Program.cs
+++ src/HeartScript.Cli/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq.Expressions;
+using HeartScript.Compiling;
 using HeartScript.Expressions;
 using HeartScript.Parsing;
 
@@ -20,11 +22,21 @@ namespace HeartScript.Cli
                 var ctx = new ParserContext(infix);
                 var node = ExpressionPattern.Parse(operators, ctx);
 
-                if (node == null && ctx.Exception != null)
-                    throw ctx.Exception;
+                if (node == null)
+                {
+                    if (ctx.Exception != null)
+                        throw ctx.Exception;
+                    else
+                        throw new Exception($"{infix} failed to parse");
+                }
 
                 Console.WriteLine("Output");
                 Console.WriteLine(node);
+
+                var compiledExpression = ExpressionNodeCompiler.Compile((ExpressionNode)node);
+                var compiledFunction = Expression.Lambda<Func<float>>(compiledExpression).Compile();
+                Console.WriteLine("Result");
+                Console.WriteLine(compiledFunction());
             }
             catch (Exception ex)
             {
---
+++ src/HeartScript/Compiling/ExpressionNodeCompiler.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using HeartScript.Expressions;
+
+namespace HeartScript.Compiling
+{
+    public static class ExpressionNodeCompiler
+    {
+        const int Nullary = 0;
+        const int Prefix = 1;
+        const int Postfix = 2;
+        const int Infix = 3;
+
+        private readonly static Func<ExpressionNode, Expression?>[][] s_nodeCompilers;
+
+        static ExpressionNodeCompiler()
+        {
+            s_nodeCompilers = new Func<ExpressionNode, Expression?>[4][];
+
+            s_nodeCompilers[Nullary] = new Func<ExpressionNode, Expression?>[]
+            {
+                ParseFloat,
+                ParseInt,
+            };
+
+            s_nodeCompilers[Prefix] = new Func<ExpressionNode, Expression?>[]
+            {
+            };
+
+            s_nodeCompilers[Postfix] = new Func<ExpressionNode, Expression?>[]
+            {
+            };
+
+            s_nodeCompilers[Infix] = new Func<ExpressionNode, Expression?>[]
+            {
+                CompileBinary,
+            };
+        }
+
+        static int GetFixity(ExpressionNode node)
+        {
+            int index = 0;
+
+            if (node.HaveLeft)
+                index |= 1 << 0;
+            if (node.HaveRight)
+                index |= 1 << 1;
+
+            return index;
+        }
+
+        public static Expression Compile(ExpressionNode node)
+        {
+            var fixityCompilers = s_nodeCompilers[GetFixity(node)];
+
+            Expression? compiled = null;
+            foreach (var compiler in fixityCompilers)
+            {
+                compiled = compiler(node);
+                if (compiled != null)
+                    break;
+            }
+
+            if (compiled == null)
+                throw new ArgumentException($"{nameof(node)} does not have a matching compiler");
+
+            return compiled;
+        }
+
+        static Expression? ParseFloat(ExpressionNode node)
+        {
+            if (float.TryParse(node.Value, out float value))
+                return Expression.Constant(value);
+
+            return null;
+        }
+
+        static Expression? ParseInt(ExpressionNode node)
+        {
+            if (int.TryParse(node.Value, out int value))
+                return Expression.Constant(value);
+
+            return null;
+        }
+
+        static Expression? CompileBinary(ExpressionNode node)
+        {
+            var left = Compile((ExpressionNode)node.Children[0]);
+            var right = Compile((ExpressionNode)node.Children[^1]);
+
+            switch (node.Value)
+            {
+                case "*": return Expression.Multiply(left, right);
+                case "/": return Expression.Divide(left, right);
+                case "+": return Expression.Add(left, right);
+                case "-": return Expression.Subtract(left, right);
+                case "&": return Expression.And(left, right);
+                case "^": return Expression.ExclusiveOr(left, right);
+                case "|": return Expression.Or(left, right);
+                default:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
