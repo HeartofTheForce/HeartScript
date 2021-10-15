5c4752ffe7c1a7cafa5a834c244e6d1192f7ea95
heart <>
2021-10-15 16:15:53 +0200

    split Nullar & Prefix tests

--- src/HeartScript.Tests/CompilerTests/CompilerNullaryTests.cs
+++ src/HeartScript.Tests/CompilerTests/CompilerNullaryTests.cs
@@ -14,39 +14,6 @@ namespace HeartScript.Tests.CompilerTests
                 ExpectedString = "1",
                 ExpectedExpression = () => 1,
             },
-            //u+
-            new ExpressionCompilerTestCase<int>()
-            {
-                Infix = "+1",
-                ExpectedString = "(u+ 1)",
-                ExpectedExpression = () => +1,
-            },
-            new ExpressionCompilerTestCase<double>()
-            {
-                Infix = "+1.5",
-                ExpectedString = "(u+ 1.5)",
-                ExpectedExpression = () => +1.5,
-            },
-            //u-
-            new ExpressionCompilerTestCase<int>()
-            {
-                Infix = "-1",
-                ExpectedString = "(u- 1)",
-                ExpectedExpression = () => -1,
-            },
-            new ExpressionCompilerTestCase<double>()
-            {
-                Infix = "-1.5",
-                ExpectedString = "(u- 1.5)",
-                ExpectedExpression = () => -1.5,
-            },
-            //~
-            new ExpressionCompilerTestCase<int>()
-            {
-                Infix = "~1",
-                ExpectedString = "(~ 1)",
-                ExpectedExpression = () => ~1,
-            },
             //real
             new ExpressionCompilerTestCase<double>()
             {
