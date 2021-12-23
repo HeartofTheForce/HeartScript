33e6ce10cc3c17840ff13fc43e59155167ad2d8a
heart <>
2021-12-23 08:55:16 +0200

    split MethodCompiler & ExpressionCompiler

--- src/HeartScript/Compiling/Emit/MethodCompiler.cs
+++ src/HeartScript/Compiling/Emit/MethodCompiler.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Reflection;
 using System.Reflection.Emit;
-using HeartScript.Ast;
 using HeartScript.Ast.Nodes;
 
 namespace HeartScript.Compiling.Emit
@@ -36,14 +35,14 @@ namespace HeartScript.Compiling.Emit
             {
                 case ReturnNode returnNode: EmitReturn(ilGenerator, basicBlock, returnNode); break;
                 case BlockNode blockNode: EmitBlock(ilGenerator, basicBlock, blockNode); break;
-                default: EmitExpression(ilGenerator, node); break;
+                default: ExpressionCompiler.EmitExpression(ilGenerator, node); break;
             }
         }
 
         private static void EmitReturn(ILGenerator ilGenerator, BasicBlock basicBlock, ReturnNode node)
         {
             if (node.Node != null)
-                EmitExpression(ilGenerator, node.Node);
+                ExpressionCompiler.EmitExpression(ilGenerator, node.Node);
 
             ilGenerator.Emit(OpCodes.Ret);
             basicBlock.Return = true;
@@ -55,184 +54,6 @@ namespace HeartScript.Compiling.Emit
             {
                 EmitStatement(ilGenerator, basicBlock, statement);
             }
-        }
-
-        private static void EmitExpression(ILGenerator ilGenerator, AstNode node)
-        {
-            switch (node)
-            {
-                case ConstantNode constantNode: EmitConstant(ilGenerator, constantNode); break;
-                case BinaryNode binaryNode: EmitBinary(ilGenerator, binaryNode); break;
-                case UnaryNode unaryNode: EmitUnary(ilGenerator, unaryNode); break;
-                case ConditionalNode conditionalNode: EmitConditional(ilGenerator, conditionalNode); break;
-                case CallNode callNode: EmitCall(ilGenerator, callNode); break;
-                case ParameterNode parameterNode: EmitParameter(ilGenerator, parameterNode); break;
-                case VariableNode variableNode: EmitVariable(ilGenerator, variableNode); break;
-                case MemberAccessNode memberAccessNode: EmitMemberAccess(ilGenerator, memberAccessNode); break;
-                default: throw new NotImplementedException();
-            }
-        }
-
-        private static void EmitConstant(ILGenerator ilGenerator, ConstantNode node)
-        {
-            if (node.Value == null)
-                throw new ArgumentException(nameof(node.Value));
-
-            if (node.Type == typeof(int))
-            {
-                ilGenerator.Emit(OpCodes.Ldc_I4, (int)node.Value);
-                return;
-            }
-
-            if (node.Type == typeof(double))
-            {
-                ilGenerator.Emit(OpCodes.Ldc_R8, (double)node.Value);
-                return;
-            }
-
-            if (node.Type == typeof(bool))
-            {
-                ilGenerator.Emit(OpCodes.Ldc_I4, (bool)node.Value ? 1 : 0);
-                return;
-            }
-
-            throw new NotImplementedException();
-        }
-
-        private static void EmitBinary(ILGenerator ilGenerator, BinaryNode node)
-        {
-            if (node.NodeType == AstType.Assign)
-            {
-                EmitExpression(ilGenerator, node.Right);
-
-                switch (node.Left)
-                {
-                    case ParameterNode parameterNode:
-                        ilGenerator.Emit(OpCodes.Starg, parameterNode.Index); break;
-                    case VariableNode variableNode:
-                        ilGenerator.Emit(OpCodes.Stloc, variableNode.Index); break;
-                    default: throw new ArgumentException("The left-hand side of an assignment must be a variable or parameter");
-                }
-
-                return;
-            }
-
-            EmitExpression(ilGenerator, node.Left);
-            EmitExpression(ilGenerator, node.Right);
-
-            switch (node.NodeType)
-            {
-                case AstType.Multiply: ilGenerator.Emit(OpCodes.Mul); break;
-                case AstType.Divide: ilGenerator.Emit(OpCodes.Div); break;
-                case AstType.Add: ilGenerator.Emit(OpCodes.Add); break;
-                case AstType.Subtract: ilGenerator.Emit(OpCodes.Sub); break;
-                case AstType.LessThanOrEqual:
-                    {
-                        ilGenerator.Emit(OpCodes.Cgt);
-                        ilGenerator.Emit(OpCodes.Ldc_I4, 0);
-                        ilGenerator.Emit(OpCodes.Ceq);
-                    }
-                    break;
-                case AstType.GreaterThanOrEqual:
-                    {
-                        ilGenerator.Emit(OpCodes.Clt);
-                        ilGenerator.Emit(OpCodes.Ldc_I4, 0);
-                        ilGenerator.Emit(OpCodes.Ceq);
-                    }
-                    break;
-                case AstType.LessThan: ilGenerator.Emit(OpCodes.Clt); break;
-                case AstType.GreaterThan: ilGenerator.Emit(OpCodes.Cgt); break;
-                case AstType.Equal: ilGenerator.Emit(OpCodes.Ceq); break;
-                case AstType.NotEqual:
-                    {
-                        ilGenerator.Emit(OpCodes.Ceq);
-                        ilGenerator.Emit(OpCodes.Ldc_I4, 0);
-                        ilGenerator.Emit(OpCodes.Ceq);
-                    }
-                    break;
-                case AstType.And: ilGenerator.Emit(OpCodes.And); break;
-                case AstType.ExclusiveOr: ilGenerator.Emit(OpCodes.Xor); break;
-                case AstType.Or: ilGenerator.Emit(OpCodes.Or); break;
-                case AstType.Assign: ilGenerator.Emit(OpCodes.Or); break;
-                default: throw new NotImplementedException();
-            }
-        }
-
-        private static void EmitUnary(ILGenerator ilGenerator, UnaryNode node)
-        {
-            EmitExpression(ilGenerator, node.Operand);
-
-            switch (node.NodeType)
-            {
-                case AstType.Convert:
-                    {
-                        if (node.Operand.Type != typeof(int) || node.Type != typeof(double))
-                            throw new ArgumentException($"Cannot convert, {node.Operand.Type} to {node.Type}");
-
-                        ilGenerator.Emit(OpCodes.Conv_R8);
-                    }
-                    break;
-                case AstType.UnaryPlus: break;
-                case AstType.Negate: ilGenerator.Emit(OpCodes.Neg); break;
-                case AstType.Not: ilGenerator.Emit(OpCodes.Not); break;
-                default: throw new NotImplementedException();
-            }
-        }
-
-        private static void EmitConditional(ILGenerator ilGenerator, ConditionalNode node)
-        {
-            var ifFalseLabel = ilGenerator.DefineLabel();
-            var endLabel = ilGenerator.DefineLabel();
-
-            EmitExpression(ilGenerator, node.Test);
-            ilGenerator.Emit(OpCodes.Brfalse, ifFalseLabel);
-
-            //true
-            EmitExpression(ilGenerator, node.IfTrue);
-            ilGenerator.Emit(OpCodes.Br, endLabel);
-
-            //false
-            ilGenerator.MarkLabel(ifFalseLabel);
-            EmitExpression(ilGenerator, node.IfFalse);
-
-            ilGenerator.MarkLabel(endLabel);
-        }
-
-        private static void EmitCall(ILGenerator ilGenerator, CallNode node)
-        {
-            if (node.Instance != null)
-                EmitExpression(ilGenerator, node.Instance);
-
-            foreach (var parameter in node.Parameters)
-            {
-                EmitExpression(ilGenerator, parameter);
-            }
-
-            ilGenerator.EmitCall(OpCodes.Call, node.MethodInfo, null);
-        }
-
-
-        private static void EmitParameter(ILGenerator ilGenerator, ParameterNode node)
-        {
-            ilGenerator.Emit(OpCodes.Ldarg, node.Index);
-        }
-
-        private static void EmitVariable(ILGenerator ilGenerator, VariableNode node)
-        {
-            ilGenerator.Emit(OpCodes.Ldloc, node.Index);
-        }
-
-        private static void EmitMemberAccess(ILGenerator ilGenerator, MemberAccessNode node)
-        {
-            if (node.Instance != null)
-                EmitExpression(ilGenerator, node.Instance);
-
-            switch (node.Member)
-            {
-                case FieldInfo fieldInfo: ilGenerator.Emit(OpCodes.Ldfld, fieldInfo); break;
-                case PropertyInfo propertyInfo: ilGenerator.EmitCall(OpCodes.Call, propertyInfo.GetMethod, null); break;
-                default: throw new NotImplementedException();
-            };
         }
     }
 }
