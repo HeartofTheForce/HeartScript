78d166c19f02304b781f9bfa4e54a12f96fc047c
heart <>
2022-01-20 18:48:54 +0200

    naming

--- src/demo.hs
+++ src/demo.hs
@@ -1,20 +1,20 @@
 void main(int[] arr)
 {
     int n = sizeof(arr);
-    QuickSort(arr, 0, n - 1);
+    quick_sort(arr, 0, n - 1);
 }
 
-void QuickSort(int[] arr, int low, int high)
+void quick_sort(int[] arr, int low, int high)
 {
     if (low < high)
     {
-        int pi = Partition(arr, low, high);
-        QuickSort(arr, low, pi - 1);
-        QuickSort(arr, pi + 1, high);
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
     }
 }
 
-int Partition(int[] arr, int low, int high)
+int partition(int[] arr, int low, int high)
 {
     int pivot = arr[high];
     int i = (low - 1);
